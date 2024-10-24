845a002e5c71be3231b530d6bf8ec4b685f71e23
Ovalkin <egorvasilevskiy585@gmail.com>
2024-10-24 21:45:05 +0500

    feat(validate): валидация для создания заказа

--- EffectiveMobile.Common.DTOs/CreateOrderDto.cs
+++ EffectiveMobile.Common.DTOs/CreateOrderDto.cs
@@ -1,13 +1,19 @@
+using System.ComponentModel.DataAnnotations;
 using EffectiveMobile.Common.EntityModel.Sqlite.Entities;
 
 namespace EffectiveMobile.Common.DTOs;
 
 public class CreateOrderDto
 {
+    [Range(0.1, 200, ErrorMessage = "Вес должен быть от 0.1кг до 200кг!")]
     public double Weight { get; set; }
 
+    [RegularExpression(@"^\D*$", ErrorMessage = "Имя района может содержать только слова")]
     public string CityDistrict { get; set; } = null!;
     
+    [DataType(DataType.DateTime, ErrorMessage = "Дата должна быть в формате yyyy-MM-ddTHH:mm:ss")]
+    [Range(typeof(DateTime), "2024-10-24 00:00:01", "2024-12-31 00:00:01", 
+        ErrorMessage = "Дата доставки должна быть в диапазоне от 2024-10-24 00:00:01 до 2024-12-31 00:00:01")]
     public DateTime DeliveryTime { get; set; }
 
     public static Order CreateEntity(CreateOrderDto dto)
